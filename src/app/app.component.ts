31bed2edbbb1486b0e30d4fb28288993b52e532e
Bastien <94696044+Bastien8131@users.noreply.github.com>
2024-01-04 13:20:56 +0100

    toggle pour switch entre dark et light mode

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -15,6 +15,7 @@ export class AppComponent {
   // calendarView = CalendarView;
   viewMenuTitle: string = "Semaine";
   showDatePickers: boolean = false;
+  themeToggle: any;
 
 
   constructor(
@@ -23,11 +24,30 @@ export class AppComponent {
 
   ngOnInit() {
     this.calendarRTAI.getCalendarData();
+    // Use matchMedia to check the user preference
+    const prefersDark = window.matchMedia('(prefers-color-scheme: dark)');
+
+    // Initialize the dark theme based on the initial
+    // value of the prefers-color-scheme media query
+    this.initializeDarkTheme(prefersDark.matches);
+
+    // Listen for changes to the prefers-color-scheme media query
+    prefersDark.addEventListener('change', (mediaQuery) => this.initializeDarkTheme(mediaQuery.matches));
   }
 
+// Check/uncheck the toggle and update the theme based on isDark
+  initializeDarkTheme(isDark: boolean | undefined) {
+    this.themeToggle = isDark;
+    this.toggleDarkTheme(isDark);
+  }
 
+  // Listen for the toggle check/uncheck to toggle the dark theme
+  toggleChange(ev: { detail: { checked: any; }; }) {
+    this.toggleDarkTheme(ev.detail.checked);
+  }
 
-
-
-
+  // Add or remove the "dark" class on the document body
+  toggleDarkTheme(shouldAdd: boolean | undefined) {
+    document.body.classList.toggle('dark', shouldAdd);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,22 +11,25 @@ import { ServiceWorkerModule } from '@angular/service-worker';
 import { registerLocaleData } from '@angular/common';
 import localeFr from '@angular/common/locales/fr';
 import {EventsDayComponent} from "./components/events-day/events-day.component";
+import {FormsModule} from "@angular/forms";
 
 registerLocaleData(localeFr, 'fr');
 
 @NgModule({
   declarations: [AppComponent, EventsDayComponent],
-  imports: [
-    BrowserModule,
-    HttpClientModule,
-    IonicModule.forRoot(),
-    AppRoutingModule,
-    ServiceWorkerModule.register('ngsw-worker.js', {
-      enabled: !isDevMode(),
-      // Register the ServiceWorker as soon as the application is stable
-      // or after 30 seconds (whichever comes first).
-      registrationStrategy: 'registerWhenStable:30000'
-    })],
+    imports: [
+        BrowserModule,
+        HttpClientModule,
+        IonicModule.forRoot(),
+        AppRoutingModule,
+        ServiceWorkerModule.register('ngsw-worker.js', {
+            enabled: !isDevMode(),
+            // Register the ServiceWorker as soon as the application is stable
+            // or after 30 seconds (whichever comes first).
+            registrationStrategy: 'registerWhenStable:30000'
+        }),
+        FormsModule
+    ],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
